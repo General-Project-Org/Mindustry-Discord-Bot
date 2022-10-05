66d477e59c8a898b04d7d40450fa1354efd05732
SecondNewtonLaw <107165614+SecondNewtonLaw@users.noreply.github.com>
2022-10-05 17:33:23 -0300

    Move source, update to use Masked.Library's Command Helper.

--- Mindustry_bot/src/Commands/CommandIndex.cs
+++ Mindustry_bot/src/Commands/CommandIndex.cs
@@ -1,70 +1,78 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+
 using Discord;
 using Discord.WebSocket;
 
 namespace MindustryManagerBot;
 
-public struct CommandBuilder
+internal class CommandIndex
 {
-    /// <summary>
-    /// Build all bot commands for a specific Guild (Discord Server)
-    /// </summary>
-    /// <param name="guild">Socket of the Guild.</param>
-    public async Task BuildFor(SocketGuild guild)
-        => await guild.BulkOverwriteApplicationCommandAsync(this.BuildCommands().ToArray());
-    /// <summary>
-    /// Build all bot commands for the whole bot | Takes two hours to apply between iterations.
-    /// </summary>
-    /// <param name="client">Bot Client.</param>
-    public async Task BuildApp(DiscordSocketClient client)
-        => await client.BulkOverwriteGlobalApplicationCommandsAsync(BuildCommands().ToArray());
+    private static Commands cmdCode = new();
 
-    private List<SlashCommandProperties> BuildCommands()
+    /// <summary>
+    /// Returns a tuple of lists that contain all the properties of all the available commands and their logic.
+    /// </summary>
+    /// <returns>Tuple of Lists containing the commands data and their logic.</returns>
+    internal static (List<SlashCommandProperties>, List<Func<SocketSlashCommand, Task>>) GetCommands()
     {
         List<SlashCommandProperties> commands = new();
+        List<Func<SocketSlashCommand, Task>> commandLogic = new();
 
         SlashCommandBuilder pingCommand = new()
         {
             Name = "ping",
             Description = "Get the ping from our bot to Discord"
         };
+        commandLogic.Add(sock => cmdCode.Ping(sock));
+
         SlashCommandBuilder startServerCommand = new()
         {
             Name = "start",
             Description = "Start the Mindustry Server."
         };
+        commandLogic.Add(sock => cmdCode.StartServer(sock));
+
         SlashCommandBuilder stopServerCommand = new()
         {
             Name = "stop",
             Description = "Start the Mindustry Server."
         };
+        commandLogic.Add(sock => cmdCode.StopServer(sock));
+
         SlashCommandBuilder loadSaveCommand = new()
         {
             Name = "load",
             Description = "Load a save in the Mindustry Server."
         };
         loadSaveCommand.AddOption("savename", ApplicationCommandOptionType.String, "The name of the save file", true);
+        commandLogic.Add(sock => cmdCode.LoadSave(sock));
+
         SlashCommandBuilder pauseCommand = new()
         {
             Name = "pause",
             Description = "Pause or resume the game depending of the value"
         };
         pauseCommand.AddOption("pause", ApplicationCommandOptionType.Boolean, "if true the server will be paused, else it will be resumed", true);
+        commandLogic.Add(sock => cmdCode.PauseGame(sock));
 
         SlashCommandBuilder srvInfoCommand = new()
         {
             Name = "serverinfo",
             Description = "Show information about the Mindustry server"
         };
+        commandLogic.Add(sock => cmdCode.ServerInformation(sock));
 
         SlashCommandBuilder executeCommand = new()
         {
             Name = "execute",
             Description = "Execute a command on the Mindustry server"
         };
+        commandLogic.Add(sock => cmdCode.ExecuteInServer(sock));
+
         executeCommand.AddOption("command", ApplicationCommandOptionType.String, "The command to execute in the Mindustry server", true);
         commands.Add(pingCommand.Build());
         commands.Add(startServerCommand.Build());
@@ -74,9 +82,6 @@ public struct CommandBuilder
         commands.Add(srvInfoCommand.Build());
         commands.Add(executeCommand.Build());
 
-
-
-
-        return commands;
+        return (commands, commandLogic);
     }
 }
--- Mindustry_bot/src/Main.cs
+++ Mindustry_bot/src/Main.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Net;
 using System.Security.Authentication;
+
 using Discord;
 using Discord.WebSocket;
+
 using Spectre.Console;
 
 namespace MindustryManagerBot;
@@ -11,6 +13,7 @@ public static class MainActivity
 {
     public static MindustryManager game_server = new();
     private static string Token { get; } = Environment.GetEnvironmentVariable("managertoken")!;
+
     public static DiscordSocketClient BotClient { get; } = new(new()
     {
         GatewayIntents = GatewayIntents.Guilds,
@@ -18,6 +21,7 @@ public static class MainActivity
         LogGatewayIntentWarnings = true,
         AlwaysDownloadUsers = true
     });
+
     internal static readonly HttpClient client = new(
     new HttpClientHandler()
     {
@@ -28,8 +32,16 @@ public static class MainActivity
         AllowAutoRedirect = true
     });
 
+    private static Masked.DiscordNet.CommandHelper _cmdHelper = new();
+
     public static async Task Main()
     {
+        (List<SlashCommandProperties> commands, List<Func<SocketSlashCommand, Task>> commandsCode) = CommandIndex.GetCommands();
+        for (int i = 0; i < commands.Count; i++)
+        {
+            _cmdHelper.AddToCommandList(commands[i], commandsCode[i]);
+        }
+
         Thread cmdListener = new(async () => await ListenForCommands())
         {
             Name = "Console Listener",
@@ -49,30 +61,44 @@ public static class MainActivity
             AnsiConsole.MarkupLine("[cyan][[Discord.NET]] {0}[/]", x.ToString().RemoveMarkup());
             return Task.CompletedTask;
         };
-        BotClient.SlashCommandExecuted += handlerMain.HandleSlashCommand;
+        BotClient.SlashCommandExecuted += _cmdHelper.GetSlashCommandHandler();
 
         // Log into the bot.
         await BotClient.LoginAsync(TokenType.Bot, Token, true);
         await BotClient.StartAsync();
         await Task.Delay(-1); // Don't close.
     }
+
     private static async Task ListenForCommands()
     {
-        CommandBuilder builder = new();
+        const ulong mindustryid = 1026842718984491068;
         while (true)
         {
             // Loop listening to console.
             switch (Console.ReadLine()!)
             {
+                case "DEV_commandtobuildcommands":
+                    AnsiConsole.MarkupLine("[[INFO]] Obtaining Guild...");
+                    SocketGuild guild = BotClient.GetGuild(mindustryid);
+                    AnsiConsole.MarkupLine("[[INFO]] Guild Obtained.");
+
+                    AnsiConsole.MarkupLine("[[INFO]] Attempting to submit the Remote Command Builder command...");
+                    Task tsk =  _cmdHelper.SubmitCommandBuilder(guild);
+                    await tsk;
+                    if (tsk.IsCompletedSuccessfully)
+                        AnsiConsole.MarkupLine("[[INFO]] Command Submitted Successfully.");
+                    if (tsk.IsFaulted)
+                        AnsiConsole.MarkupLine($"[[INFO]] Command Submit Failed, Further Error Information: {tsk.Exception}");
+
+                    break;
+
                 case "guildslashcmds":
-                    const ulong mindustryid = 1026842718984491068;
-                    new Thread(
-                        async () =>
+                    _ = Task.Run(async () =>
                         {
                             AnsiConsole.MarkupLine("[[INFO]] Building Guild Level Commands, This might take a while!");
                             try
                             {
-                                await builder.BuildFor(BotClient.GetGuild(mindustryid));
+                                await _cmdHelper.BuildFor(BotClient.GetGuild(mindustryid));
                                 AnsiConsole.MarkupLine($"[[INFO]] Commands Built for {BotClient.GetGuild(mindustryid).Name}.");
                             }
                             catch (Exception ex)
@@ -80,35 +106,42 @@ public static class MainActivity
                                 AnsiConsole.MarkupLine("[[FATAL]] Commands Failed to build with exception -> \r\n\r\n {0}", ex.ToString().RemoveMarkup());
                             }
                         }
-                    ).Start();
+                    );
                     break;
+
                 case "appslashcmds":
-                    new Thread(
-                        async () =>
+                    _ = Task.Run(async () =>
+                    {
+                        AnsiConsole.MarkupLine("[[INFO]] Building Application Level Commands, This might take a while!");
+                        try
                         {
-                            AnsiConsole.MarkupLine("[[INFO]] Building Application Level Commands, This might take a while!");
-                            try
-                            {
-                                await builder.BuildApp(BotClient);
-                                AnsiConsole.MarkupLine("[[INFO]] Commands Built at an Applcation level.");
-                            }
-                            catch (Exception ex)
-                            {
-                                AnsiConsole.MarkupLine("[[FATAL]] Commands Failed to build with exception -> \r\n\r\n {0}", ex.ToString().RemoveMarkup());
-                            }
+                            await _cmdHelper.BuildApp(BotClient);
+                            AnsiConsole.MarkupLine("[[INFO]] Commands Built at an Applcation level.");
                         }
-                    ).Start();
+                        catch (Exception ex)
+                        {
+                            AnsiConsole.MarkupLine("[[FATAL]] Commands Failed to build with exception -> \r\n\r\n {0}", ex.ToString().RemoveMarkup());
+                        }
+                    });
                     break;
+
                 case "exit":
                     AnsiConsole.MarkupLine("[yellow bold][[INFO]] Exiting Mindustry Game Manager Bot...[/]");
                     await BotClient.LogoutAsync(); // Log out of Discord.
                     await BotClient.StopAsync(); // Stop the code of the bot
-                    BotClient.Dispose(); // Dispose bot client.
-                    client.Dispose(); // Dispose HTTPClient.
+                    BotClient.Dispose(); // Dispose of the bot client.
+                    client.Dispose(); // Dispose of the HTTPClient.
                     Environment.Exit(0);
                     break;
+
                 default:
-                    AnsiConsole.MarkupLine("[red bold][[WARN]] Invalid Command.[/]");
+                    AnsiConsole.MarkupLine("[red bold][[WARN]] Invalid Command.[/]\n" +
+                        "\tAvailable Commands:\n" +
+                        "\t [green]exit[/] -> Exits the Bot, and correctly logs out.\n" +
+                        "\t [green]guildslashcmds[/] -> Builds the commands for the bot, this mode builds the commands for the server that is [bold yellow]HARDCODED[/] into the Main.cs source file.\n" +
+                        "\t [yellow]appslashcmds[/] -> Builds the commands for the bot, this mode allows for [red bold]ANY[/] server in which the bot is in to access them.\n" +
+                        "\t [red]DEV_commandtobuildcommands[/] -> Builds a command that allows ANY member of the server the bot is in to build the commands of the bot remotely.");
+
                     break;
             }
         }
